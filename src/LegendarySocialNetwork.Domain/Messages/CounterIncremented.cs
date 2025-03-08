43ea58272312fff3f74dbd0b1e9522b187247398
Oleg tarnarutsky <olegtar83@gmail.com>
2025-03-08 10:58:29 +0300

    redundant message

--- src/LegendarySocialNetwork.Domain/Messages/CounterIncremented.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LegendarySocialNetwork.Domain.Messages
-{
-    public class CounterIncremented
-    {
-        public required string From { get; set; }
-        public required string To { get; set; }
-        public required string Text { get; set; }
-    }
-}
